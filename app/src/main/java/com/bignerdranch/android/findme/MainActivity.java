51e4e773f4c61f595da6b404c9f7ffe2b8c79d61
k-andrey01 <russia_andreykazantsev2001@mail.ru>
2022-05-17 18:20:50 +0300

    Fix bugges

--- app/src/main/java/com/bignerdranch/android/findme/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/findme/MainActivity.java
@@ -6,6 +6,9 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -15,43 +18,65 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
-    StringBuilder sbGPS = new StringBuilder();
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.SEND_SMS, Manifest.permission.ACCESS_COARSE_LOCATION},
+                123);
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         final Animation animAlpha = AnimationUtils.loadAnimation(this, R.anim.alpha);
         Button btnFind = findViewById(R.id.find_btn);
-        btnFind.setOnClickListener(new Button.OnClickListener(){
+        btnFind.setOnClickListener(new Button.OnClickListener() {
+
             @SuppressLint("MissingPermission")
             @Override
             public void onClick(View view) {
                 view.startAnimation(animAlpha);
 
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 10, locationListener);
-
-                String messageText = showLocation(locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER));
+                String messageText = showLocation(locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER));
                 System.out.println(messageText);
-                SmsManager.getDefault()
-                        .sendTextMessage("+79273527207", null, messageText, null, null);
+                SmsManager sms = SmsManager.getDefault();
+                ArrayList<String> parts = sms.divideMessage(messageText);
+                sms.sendMultipartTextMessage("+79273527207", null, parts, null, null);
+                //SmsManager.getDefault().sendTextMessage("+79273527207", null, messageText, null, null);
+                System.out.println("jjhhghg");
             }
         });
     }
 
+    @SuppressLint("MissingPermission")
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                0, 0, locationListener);
+    }
+
     private LocationListener locationListener = new LocationListener() {
 
+        @SuppressLint("MissingPermission")
         @Override
         public void onLocationChanged(Location location) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
+                    0, 0, locationListener);
         }
 
         @Override
@@ -68,9 +93,20 @@ public class MainActivity extends AppCompatActivity {
     };
 
     private String showLocation(Location location) {
-        return String.format(
-                "Широта: %1$.4f\nДолгота: %2$.4f\nВремя: %3$tF %3$tT",
-                location.getLatitude(), location.getLongitude(), new Date(
-                        location.getTime()));
+        String addressName = null;
+        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+        List<Address> addresses;
+        try {
+            addresses = gcd.getFromLocation(location.getLatitude(),
+                    location.getLongitude(), 1);
+            if (addresses.size() > 0) {
+                addressName = addresses.get(0).getAddressLine(0);
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "Широта: "+location.getLatitude()+"\nДолгота: "+location.getLongitude()+"\n"+addressName;
     }
 }
+
